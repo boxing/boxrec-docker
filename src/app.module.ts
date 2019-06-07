760dd543b0118cfb4b418caa77f9186636c5395e
Mike DiDomizio <mikedidomizio@gmail.com>
2019-06-07 09:32:05 -0400

    Add ratings module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import {MiddlewareConsumer, Module, NestModule, RequestMethod} from '@nestjs/common';
-import {BoxerController} from './pages/boxer/boxer.controller';
 import {BuildCookieJarMiddleware} from './middleware/build-cookie-jar.middleware';
 import {IsLoggedInMiddleware} from './middleware/check-if-logged-in.middleware';
+import {BoxerController} from './pages/boxer/boxer.controller';
 import {LoginController} from './pages/login/login.controller';
+import {RatingsController} from './pages/ratings/ratings.controller';
 
 @Module({
     imports: [],
-    controllers: [LoginController, BoxerController],
+    controllers: [LoginController, BoxerController, RatingsController],
     providers: [],
 })
 export class AppModule implements NestModule {
